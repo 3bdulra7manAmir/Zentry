53ea3b54a30488c39329045aa3dec97808a9f255
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-03-17 22:39:14 +0200

    app styles

--- lib/Core/constants/app_strings.dart
+++ lib/Core/constants/app_strings.dart
@@ -4,5 +4,4 @@ abstract class AppStrings
 
   static const hello = "Hello";
   static const hitMe = "Hit Me";
-  static const loremIpsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.";
 }
---
+++ lib/Core/constants/app_styles.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class AppStyles
+{
+  AppStyles._();
+
+  static TextStyle textStyle24 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle textStyle22 = TextStyle(
+    fontSize: 22.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle textStyle20 = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle textStyle18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle textStyle16 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle textStyle14 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle textStyle12 = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle textStyle10 = TextStyle(
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+    fontFamily: 'Roboto',
+  );
+}
