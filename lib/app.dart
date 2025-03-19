0b15edac7e899a22410ef0d54a3282ed7bfd2fa8
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-03-20 00:27:23 +0200

    app.dart

---
+++ lib/app.dart
@@ -0,0 +1,31 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:test_app/config/router/app_router.dart';
+
+class TestApp extends StatelessWidget
+{
+  const TestApp({super.key});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, child)
+      {
+        return DevicePreview(
+          enabled: false,
+          builder: (context) => MaterialApp.router(
+            builder: DevicePreview.appBuilder,
+            locale: DevicePreview.locale(context),
+            routerConfig: AppRouter.router,
+        
+            debugShowCheckedModeBanner: false,
+            //useInheritedMediaQuery: true,
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,11 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:test_app/config/router/app_router.dart';
+import 'package:test_app/app.dart';
 
 void main()
 {
   WidgetsFlutterBinding.ensureInitialized();
   
-  runApp(
-    ScreenUtilInit(
-      designSize: const Size(375, 812),
-      builder: (context, child) => DevicePreview(
-        builder: (context) => const TestApp(),
-        enabled: true,
-      ),
-    ),
-  );
+  runApp(const TestApp(),);
 }
 
-class TestApp extends StatelessWidget
-{
-  const TestApp({super.key});
 
-  @override
-  Widget build(BuildContext context)
-  {
-    return DevicePreview(
-      enabled: false,
-      builder: (context) => MaterialApp.router(
-        useInheritedMediaQuery: true,
-        builder: DevicePreview.appBuilder,
-        locale: DevicePreview.locale(context),
-        routerConfig: AppRouter.router,
-
-        debugShowCheckedModeBanner: false,
-      ),
-    );
-  }
-}
