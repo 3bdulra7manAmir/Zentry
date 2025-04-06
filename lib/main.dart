d1035f00aec9a44fe204f348bfa0773240a4bad5
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-04-06 15:59:22 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
-// ignore_for_file: unused_import
-
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 
 void main()
 {
-  //WidgetsFlutterBinding.ensureInitialized();
-  
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const ProviderScope(child: TestApp()),);
 }
