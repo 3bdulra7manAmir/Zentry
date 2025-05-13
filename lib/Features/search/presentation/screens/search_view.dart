acae784cb501149c6dc7bc42bcbfb174102cfdbe
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-05-13 17:10:04 +0300

    search view and following business card

--- lib/Features/search/presentation/screens/search_view.dart
+++ lib/Features/search/presentation/screens/search_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../config/l10n/generated/app_localizations.dart';
 import '../../../../config/themes/color_system/colors_manager/app_colors.dart';
@@ -8,47 +9,88 @@ import '../../../../core/constants/app_sizes.dart';
 import '../../../../core/constants/app_styles.dart';
 import '../../../../core/widgets/app_listview_builder.dart';
 import '../../../../core/widgets/app_search_appbar.dart';
+import '../providers/search_providers.dart';
 import '../widgets/search_related_result_card.dart';
 
-class SearchView extends StatelessWidget
+class SearchView extends ConsumerWidget
 {
   const SearchView({super.key});
 
   @override
-  Widget build(BuildContext context)
+  Widget build(BuildContext context, WidgetRef ref)
   {
+    final searchResults = ref.watch(searchResultsProvider);
     return Scaffold(
-      appBar: const CustomSearchAppBar(locationIcon: true,),
-      body:
-          Padding(
-            padding: AppPadding.kAppFormPadding,
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      appBar: const CustomSearchAppBar(locationIcon: true),
+      body: Padding(
+        padding: AppPadding.kAppFormPadding,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:
+            [
+              AppSizes.size10.verticalSpace,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children:
                 [
-                  AppSizes.size10.verticalSpace,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children:
-                    [
-                      Text(AppLocalizations.of(context).categories, style: AppStyles.textStyle16(fontWeight: AppFontWeights.semiBoldWeight, fontColor: AppColors.color.kAgainTextDark),),
-                      Text(AppLocalizations.of(context).clear, style: AppStyles.textStyle14(fontWeight: AppFontWeights.semiBoldWeight, fontColor: AppColors.color.kForgetPasswordUnderLine),),
-                    ],
+                  Text(AppLocalizations.of(context).categories, style: AppStyles.textStyle16(
+                      fontWeight: AppFontWeights.semiBoldWeight,
+                      fontColor: AppColors.color.kAgainTextDark,
+                    ),
                   ),
-                  AppSizes.size3.verticalSpace,
-                  Text(AppLocalizations.of(context).relatedResults, style: AppStyles.textStyle12(fontColor: AppColors.color.kSecondarySemiGreyText),),
-                  AppSizes.size16.verticalSpace,
-                  //const SearchRelatedResultCard(),
-                  AppListviewBuilder(
-                    itemBuilder: (context, index) => const SearchRelatedResultCard(),
-                    itemCount: 11,
-                    separatorBuilder: (context, index) => AppSizes.size15.verticalSpace,
+                  Text(AppLocalizations.of(context).clear, style: AppStyles.textStyle14(
+                      fontWeight: AppFontWeights.semiBoldWeight,
+                      fontColor: AppColors.color.kForgetPasswordUnderLine,
+                    ),
                   ),
                 ],
               ),
-            ),
+              AppSizes.size3.verticalSpace,
+              Text(AppLocalizations.of(context).relatedResults, style: AppStyles.textStyle12(fontColor: AppColors.color.kSecondarySemiGreyText,),),
+              AppSizes.size16.verticalSpace,
+              Expanded(
+                child: searchResults.when(
+                  data: (categories)
+                  {
+                    if (categories.isEmpty)
+                    {
+                      return Center(
+                        child: Text(AppLocalizations.of(context).search, style: AppStyles.textStyle14(
+                            fontColor: AppColors.color.kSecondarySemiGreyText,
+                            fontWeight: AppFontWeights.regularWeight,
+                          ),
+                        ),
+                      );
+                    }
+                    return AppListviewBuilder(
+                      padding: AppPadding.kAppFormPadding,
+                      itemCount: categories.length,
+                      separatorBuilder: (context, index) => AppSizes.size12.verticalSpace,
+                      itemBuilder: (context, index)
+                      {
+                        final category = categories[index];
+                        return SearchRelatedResultCard(category: category);
+                      },
+                    );
+                  },
+                  loading: () => const Center(child: CircularProgressIndicator()),
+                  error:
+                      (error, stack) => Center(
+                        child: Text(
+                          'Error: $error',
+                          style: AppStyles.textStyle14(
+                            fontColor: AppColors.color.kSecondarySemiGreyText,
+                            fontWeight: AppFontWeights.regularWeight,
+                          ),
+                        ),
+                      ),
+                ),
+              ),
+            ],
           ),
-      );
+        ),
+      ),
+    );
   }
 }
