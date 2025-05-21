315427d031de25f18b10b6f91c9640eb6284d373
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-05-21 19:40:06 +0300

    enhancments

--- lib/Features/auth/login/presentation/controllers/checkbox_provider.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final checkboxValueProvider = StateProvider<bool>((ref) => false);
--- lib/Features/auth/login/presentation/controllers/obsecure_text_provider.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final obscurePasswordProvider = StateProvider<bool>((ref) => true);
-final obscurePasswordProvider2 = StateProvider<bool>((ref) => true);
